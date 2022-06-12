de25aca83361080f6c96a68e68047a6fc3e8b1c2
David Razmadze <razd45@gmail.com>
2022-06-12 11:05:17 -0400

    Create StoryView

--- IGClone/ContentView.swift
+++ IGClone/ContentView.swift
@@ -11,7 +11,9 @@ struct ContentView: View {
   
   var body: some View {
     VStack {
-      Header()
+      HeaderView()
+      
+      StoryView()
       
       Spacer()
     }
@@ -26,7 +28,7 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 /// Contains Logo, plus, heart, & message
-struct Header: View {
+struct HeaderView: View {
   var body: some View {
     HStack {
       Image(systemName: "rectangle.inset.filled.and.person.filled")
@@ -41,5 +43,38 @@ struct Header: View {
     }
     .padding(.horizontal, 16)
     .padding(.vertical, 8)
+  }
+}
+
+struct StoryView: View {
+  
+  // Parameters to pass into `StoryView()`
+  var image = "person_1"
+  var name = "John Smith"
+  
+  // Private variables
+  private var imageSize: CGFloat = 60
+  private var lineWidth: CGFloat = 2.5
+  private var overlayColors: [Color] = [ .blue, .purple, .red, .pink, .yellow, .orange]
+  
+  var body: some View {
+    VStack {
+      VStack {
+        Image(image)
+          .resizable()
+          .frame(width: imageSize, height: imageSize)
+          .cornerRadius(imageSize)
+      }
+      .overlay(
+        Circle()
+          .stroke(
+            LinearGradient(colors: overlayColors, startPoint: .topLeading, endPoint: .bottomTrailing), lineWidth: lineWidth)
+          .frame(width: imageSize + 8, height: imageSize + 8)
+      )
+      .frame(width: imageSize + 10, height: imageSize + 10)
+      
+      Text(name)
+        .font(.caption)
+    }
   }
 }
