82d9b9f9fbec423a1f15d22811a0c52c23c6feb1
David Razmadze <razd45@gmail.com>
2022-06-12 10:45:18 -0400

    Initial Commit

---
+++ IGClone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IGClone
+//
+//  Created by David Razmadze on 6/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IGClone/IGCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  IGCloneApp.swift
+//  IGClone
+//
+//  Created by David Razmadze on 6/12/22.
+//
+
+import SwiftUI
+
+@main
+struct IGCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
