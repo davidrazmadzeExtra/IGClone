59a5377b4c0667d88585b69d0e2e12da9fff184b
David Razmadze <razd45@gmail.com>
2022-06-12 10:49:41 -0400

    Create outline

--- IGClone/ContentView.swift
+++ IGClone/ContentView.swift
@@ -8,14 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  
+  var body: some View {
+    VStack {
+      Text("1. Top Nav Bar")
+      Text("2. Stories Tray")
+      Text("3. Post with Info + User Header")
+      Text("4. Bottom Nav Bar")
     }
+  }
+  
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
